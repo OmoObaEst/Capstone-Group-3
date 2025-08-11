d523d2d713bf480ff4b3640f16960b233534818f
Estther <esanni20079@gmail.com>
2025-08-11 09:46:43 -0400

    Initial Commit

---
+++ Capstone Group 3/Capstone_Group_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_Group_3App.swift
+//  Capstone Group 3
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_Group_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capstone Group 3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capstone Group 3
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
