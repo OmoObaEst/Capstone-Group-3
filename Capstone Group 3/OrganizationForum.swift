2e36d84535d1ccee2a606e5b3536d924cce99f68
Jovie <marcemg101@gmail.com>
2025-08-11 15:31:48 -0400

    added first iteration pages

---
+++ Capstone Group 3/OrganizationForum.swift
@@ -0,0 +1,18 @@
+//
+//  OrganizationForum.swift
+//  Capstone Group 3
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+struct OrganizationForum: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrganizationForum()
+}
