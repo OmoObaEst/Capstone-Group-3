5431cbc0b231eb9b2231b65c28804f27a2e675ef
Elizabeth Saka <esaka28@andover.edu>
2025-08-12 12:47:00 -0400

    I'm starting to allow the users to change their profile picture. Need to create a new view/file

--- Capstone Group 3/UserProfile.swift
+++ Capstone Group 3/UserProfile.swift
@@ -9,26 +9,56 @@ import SwiftUI
 
 struct UserProfile: View {
     var body: some View {
+        
+        @State var isPickerShowing = false
+        
         VStack (spacing: 25){
-            Text("@Username")
+            Button {
+                //show the image pciker
+                isPickerShowing = true
+                
+            } label: {
+                Text("Change Photo")
+            }//end of button
+            
+            
+            
+            
+            
+            
+            
+            
+            Text("Username")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text ("#Skills")
-                .font(.headline)
-        }//end of Vstack
-                
-        VStack (spacing: 60){
-                Text("Hours")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Organizations")
-                    .font(.title)
-                    .fontWeight(.semibold)
-
+            HStack {
+                Text ("#Skills")
+                    .font(.headline)
             }//end of HStack
+        }//end of Vstack
+        // the top of the screen with the username and skills
+        Spacer().frame(height: 175)
+    
+        VStack (spacing: 100){
+            Text("Hours")
+                .font(.title)
+                .fontWeight(.semibold)
+            Text("Organizations")
+                .font(.title)
+                .fontWeight(.semibold)
+
+            }//end of VStack
+        //the bottom of the screen with the hours and the organizations
+        .frame(maxWidth: .infinity, alignment: .leading) // pushes the text "hours" and "orgs" to the side
+        .padding () //makes it look less akward on the sides
+        .sheet(isPresented: $isPickerShowing, onDismiss: nil) {
+            //image picker)
+            
+        } //end of image picker)
     }//end of some View
 }//end of UserProfile: View
 
 #Preview {
     UserProfile()
 }
+
