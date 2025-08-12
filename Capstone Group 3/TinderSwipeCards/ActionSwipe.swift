95d9cab589325144a8040aa1543ee284d2e93f23
Jovie <marcemg101@gmail.com>
2025-08-12 12:01:04 -0400

    added new folder & pages to TinderSwipeCards

---
+++ Capstone Group 3/TinderSwipeCards/ActionSwipe.swift
@@ -0,0 +1,18 @@
+//
+//  ActionSwipe.swift
+//  Capstone Group 3
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct ActionSwipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionSwipe()
+}
---
+++ Capstone Group 3/TinderSwipeCards/BillionOysterSwipe.swift
@@ -0,0 +1,18 @@
+//
+//  BillionOysterSwipe.swift
+//  Capstone Group 3
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct BillionOysterSwipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BillionOysterSwipe()
+}
