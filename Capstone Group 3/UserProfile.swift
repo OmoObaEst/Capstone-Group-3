0e27fb18b918d744ab4ad615e912c78cf04efdf6
Elizabeth Saka <esaka28@andover.edu>
2025-08-12 09:56:41 -0400

    I added all the text needed for the user profile

--- Capstone Group 3/UserProfile.swift
+++ Capstone Group 3/UserProfile.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 struct UserProfile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack (spacing: 25){
+            Text("@Username")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text ("#Skills")
+                .font(.headline)
+        }//end of Vstack
+                
+        VStack (spacing: 60){
+                Text("Hours")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Organizations")
+                    .font(.title)
+                    .fontWeight(.semibold)
+
+            }//end of HStack
+    }//end of some View
+}//end of UserProfile: View
 
 #Preview {
     UserProfile()
