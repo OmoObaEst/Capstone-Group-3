6a158402bd987eff3ad4ab71317a326c2a70054d
Elizabeth Saka <esaka28@andover.edu>
2025-08-13 13:46:06 -0400

    Added an image

--- Capstone Group 3/UserProfile.swift
+++ Capstone Group 3/UserProfile.swift
@@ -16,57 +16,78 @@ struct UserProfile: View {
                 var hours: Int = 0
                 var skills: String = "Skills"
         */
-       
-        ZStack{
+        ScrollView{
+            ZStack{
                 Color(.black)
                     .ignoresSafeArea()
-            VStack {
-                HStack (spacing: 25){
-                    
-                    Text("Username")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
-                    
-                    Button {
-                        //show the image picker
-                        isPickerShowing = true
+                VStack {
+                    HStack (spacing: 25){
                         
-                    } label: {
-                        Text("Change Photo")
-                    }//end of button
+                        Text("Username")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        
+                        Button {
+                            //show the image picker
+                            isPickerShowing = true
+                            
+                        } label: {
+                            Text("Change Photo")
+                        }//end of button
+                        
+                        .padding ()
+                    }//end of Hstack
                     
-                    .padding ()
-                }//end of Hstack
-                
-                // the top of the screen with the username and skills
-                
-                .offset(y: -200)
-                
-                HStack {
-                    Text("Organizations")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
-                    Spacer ()
-                    Text("Hours")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
-                }//end of the HStack
-                
-                
-                
-                //the bottom of the screen with the hours and the organizations
-                
-                
-                /*.sheet(isPresented: $isPickerShowing, onDismiss: nil) {
-                 //image picker)
-                 ImagePicker ()
-                 } //end of image picker)*/
-                
-            }//end of vstack
-        }//end of ZStack
+                    // the top of the screen with the username and skills
+                    
+                    //.offset(y: -200)
+                    
+                    HStack {
+                        Text("Organizations")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        Spacer ()
+                        Text("Hours")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }//end of the HStack
+                    HStack {
+                        
+                            Text("Salvation Army ")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                            Image("vol")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(Color.white)
+                                .cornerRadius(3)
+                                .padding()
+                                .background(Rectangle()
+                                    .foregroundColor(.white))
+                                .cornerRadius(15)
+                                .shadow(radius: 15)
+                                .padding()
+                            
+                        Text("5 Hours")
+                            .foregroundColor(Color.white)
+                    }//end of HStack
+                    
+                    
+                    //the bottom of the screen with the hours and the organizations
+                    
+                    
+                    /*.sheet(isPresented: $isPickerShowing, onDismiss: nil) {
+                     //image picker)
+                     ImagePicker ()
+                     } //end of image picker)*/
+                    
+                }//end of vstack
+            }//end of ZStack
+        }.background(Color.black)//end of scroll view
     }//end of some View
 }//end of UserProfile: View
 
