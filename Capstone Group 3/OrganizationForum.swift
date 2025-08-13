a7288dbc21154952bffb71d3c708b57d8efa6f5f
joan <nustellapham@gmail.com>
2025-08-13 13:10:11 -0400

    finished organization p[rofile

--- Capstone Group 3/OrganizationForum.swift
+++ Capstone Group 3/OrganizationForum.swift
@@ -9,20 +9,7 @@ import SwiftUI
 
 struct OrganizationForum: View {
     var body: some View {
-        VStack{
-            Text("Salvation Army")
-            Link("Website", destination: URL(string: "http://www.salvationarmyusa.org/")!)
-            Text("Locations")
-            Image("salvationarmymap")
-                .resizable(resizingMode: .stretch)
-                .frame(width: 300.0, height: 250.0)
-            Text("Calender")
-            Image("calender")
-                .resizable(resizingMode: .stretch)
-                .frame(width: 300.0, height: 250.0)
-            
-            Text("PSA")
-        }
+       
     }
 }
 
