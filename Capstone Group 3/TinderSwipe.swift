7df73c3b70d01f1dce71fb5283e6e74109a27539
Jovie <marcemg101@gmail.com>
2025-08-14 10:39:09 -0400

    adding salvation army details on TinderSwipe

--- Capstone Group 3/TinderSwipe.swift
+++ Capstone Group 3/TinderSwipe.swift
@@ -53,32 +53,32 @@ struct TinderSwipe: View {
                     .ignoresSafeArea() // color of the background
                 
                 VStack(alignment: .leading, spacing:20) {
-                    Image("Joy")
+                    Image("salvationarmy")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
 
                         .cornerRadius(15)
     // Text for TinderSwipe
                     HStack(spacing:20) {
-                        Text("Joy Buolamwini")
+                        Text("Salvation Army")
                             .font(.title)
                             .fontWeight(.bold)
 
                         Spacer()
 
-                        Text("Poet of Code")
+                        Text("Thrift Store & Faith Organization")
 
                     } // end of HStack
 
                     
-                    Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+                    Text("The Salvation Army is an international Christian charitable organization with a mission to preach the gospel of Jesus Christ and meet human needs in His name without discrimination.")
                     
-                    Text("Location: ")
-                    Text("Times: ")
-                    Text("Ages: ")
+                    Text("Location: Varies")
+                    Text("Times: 9AM - 5PM")
+                    Text("Ages: 18+")
                     Text("Skills Needed: ")
                     
-                    Text("Would you be interested in joining [insert organization name]?")
+                    Text("Would you be interested in joining Salvation Army?")
                         .font(.title3)
                         .fontWeight(.semibold)
                     
